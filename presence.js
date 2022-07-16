ecfc87d71deaa3c98f2a66738c0ed2b328f68da8
zephyr-f <79936180+zephyr-f@users.noreply.github.com>
2022-07-16 22:51:42 +0300

    data from integro

--- presence.js
+++ presence.js
@@ -8,52 +8,49 @@ sliderImages.oninput = function () {
   output.innerHTML = this.value;
 };
 
-const group = {
-  kids: {
-    1: {
-      fname: 'יובל',
-      lname: 'פורשר',
-      image: 'download.jfif',
-    },
-    2: {
-      fname: 'דוד',
-      lname: 'מרציאנו',
-      image: 'download(1).jfif',
-    },
-    3: {
-      fname: 'דני',
-      lname: 'ישראלי',
-      image: 'download(2).jfif',
-    },
-    4: {
-      fname: 'יעל',
-      lname: 'פז',
-      image: 'download(3).jfif',
-    },
-    5: {
-      fname: 'עודד',
-      lname: 'קטש',
-      image: 'download(4).jfif',
-    },
-  },
-};
-let kidsContainer = document.getElementById('kids-container');
-for (let i in group.kids) {
-  const kid = group.kids[i];
-  kidsContainer.innerHTML +=
-    `<div id="${i}" class="kid-card-BG" style="background:  #a8ff78 url('${kid.image}')"` +
-    // ontouchend="kidTouch(this)"
-    `onclick="kidTouch(this)">
+async function getGroupStatus() {
+  const options = {
+    method: 'GET',
+    url: 'https://hook.us1.make.com/uq4x35bqois6uwwsk1fyxk37x7re6160',
+    headers: { 'content-type': 'application/json' },
+  };
+  await axios
+    .request(options)
+    .then(res => {
+      console.log(res.data);
+      group = res.data;
+    })
+    .catch(err => {
+      console.error(err);
+    });
+  let kidsContainer = document.getElementById('kids-container');
+
+  for (let i in group) {
+    const kid = group[i].kid;
+    console.log(kid);
+    kidsContainer.innerHTML +=
+      `<div id="${kidNum}" class="kid-card-BG" style="background:  #a8ff78 url('${kid.image}')"` +
+      // ontouchend="kidTouch(this)"
+      `onclick="kidTouch(this)">
 <div class="name-contain">
 <p class="name-in-frame">${kid.fname}<br>${kid.lname}</p>
 </div>
 </div>`;
+  }
 }
+getGroupStatus();
 
 function kidTouch(kidCard) {
-  // console.log(kidCard);
+  console.log(kidCard);
   // alert(kidCard.id);
   kidCard.classList.toggle('kid-card-BG_checked');
+  const isChecked = kidCard.classList.contains('kid-card-BG_checked');
+  console.log(isChecked);
+
+  axios.post('https://hook.us1.make.com/uq4x35bqois6uwwsk1fyxk37x7re6160', {
+    kidNum: kidCard.id,
+    checked: isChecked,
+  });
 }
 // function kidTouch(kidNum) {
 //   document
